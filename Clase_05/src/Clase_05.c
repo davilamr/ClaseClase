85ad71a368cbc33f3b221a3dc357696e66d946a7
profesor <profesor@LAB00PC00>
2019-09-03 19:24:14 -0300

    Inicio clase 05

---
+++ Clase_05/src/Clase_05.c
@@ -0,0 +1,52 @@
+/*
+ ============================================================================
+ Name        : Clase_05.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#define QTY_EMPLEADOS 1000
+int main(void) {
+
+	int i;
+	int edadesEmpleados[QTY_EMPLEADOS];
+
+	if(initArrayInt(edadesEmpleados,QTY_EMPLEADOS,521) == 0)
+	{
+		imprimirArray(edadesEmpleados,QTY_EMPLEADOS);
+	}
+
+	return EXIT_SUCCESS;
+}
+
+int initArrayInt(int array[],int limite,int valor )
+{
+	int i;
+	int retorno = -1;
+	if(array != NULL && limite > 0)
+	{
+		retorno = 0;
+		for(i=0;i<limite;i++)
+		{
+			array[i]=valor;
+		}
+
+	}
+	return retorno;
+}
+
+
+
+
+
+
+
+
+
+
+
