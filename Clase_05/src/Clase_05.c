b1fe5fc3e2a26109b599c59b49466c0a1ad59376
profesor <profesor@LAB00PC00>
2019-09-03 22:29:43 -0300

    add Calcula Maximo

--- Clase_05/src/Clase_05.c
+++ Clase_05/src/Clase_05.c
@@ -11,7 +11,7 @@
 #include <stdio.h>
 #include <stdio_ext.h>
 #include <stdlib.h>
-#define QTY_EMPLEADOS 1000
+#define QTY_EMPLEADOS 10
 int imprimeArrayInt(int array[],int limite);
 int initArrayInt(int array[],int limite,int valor);
 int getArrayInt(	int array[],
@@ -21,29 +21,39 @@ int getArrayInt(	int array[],
 					int minimo,
 					int maximo,
 					int reintentos);
-int maximoArrayInt(int array[],int limite,int cantidaElementos,int *pResultado);
-int minimoArrayInt(int array[],int limite,int cantidaElementos,int *pResultado);
-int promedioArrayInt(int array[],int limite,int cantidaElementos,float *pResultado);
+int maximoArrayInt(int array[],int limite,int *pResultado);
+int minimoArrayInt(int array[],int limite,int *pResultado);
+int promedioArrayInt(int array[],int limite,float *pResultado);
 int getInt(	int *pResultado,
 			char *pMensaje,
 			char *pMensajeError,
 			int minimo,
 			int maximo,
 			int reintentos);
+int ordenarArrayInt(int array[],int limite);
 
 int main(void)
 {
-	int edadesEmpleados[QTY_EMPLEADOS];
+	int edadesEmpleados[QTY_EMPLEADOS] =  {22,1,44,2,1};
+	int cantidadDatos = 5 ;
 	int test;
-	if(getInt(&test,"Int?\n","Error\n",0,10,2) == 0)
+
+	//cantidadDatos = getArrayInt(edadesEmpleados,QTY_EMPLEADOS,"Edad?\n","Error\n",0,200,2);
+	if(cantidadDatos > 0)
 	{
-		printf("---> %d\n\n\n\n",test);
+		imprimeArrayInt(edadesEmpleados,cantidadDatos);
+		ordenarArrayInt(edadesEmpleados,cantidadDatos);
+		imprimeArrayInt(edadesEmpleados,cantidadDatos);
+		maximoArrayInt(edadesEmpleados,cantidadDatos,&test);
 	}
 
+
+/*
 	if(initArrayInt(edadesEmpleados,QTY_EMPLEADOS,10) == 0)
 	{
 		imprimeArrayInt(edadesEmpleados,QTY_EMPLEADOS);
 	}
+*/
 	return EXIT_SUCCESS;
 }
 
@@ -70,6 +80,7 @@ int imprimeArrayInt(int array[],int limite )
 	if(array != NULL && limite > 0)
 	{
 		retorno = 0;
+		printf("\n\n-------\n");
 		for(i=0;i<limite;i++)
 		{
 			printf("%d\n",array[i]);
@@ -98,16 +109,90 @@ int getInt(	int *pResultado,
 			retorno = 0;
 			break;
 		}
+		printf("%s",pMensajeError);
 		reintentos--;
 	}while(reintentos >= 0);
 	return retorno;
 }
 
 
+int getArrayInt(	int array[],
+					int limite,
+					char *pMensaje,
+					char *pMensajeError,
+					int minimo,
+					int maximo,
+					int reintentos)
+{
+	int i=0;
+	int buffer;
+	char respuesta = 'n';
+	int retorno = -1;
+	if(array != NULL && limite > 0)
+	{
+		do
+		{
+			if(getInt(	&buffer,
+						pMensaje,
+						pMensajeError,
+						minimo,
+						maximo,
+						reintentos) == 0)
+			{
+				array[i] = buffer;
+				i++;
+				limite--;
+			}
+			printf("Continuar? (s/n)");
+			__fpurge(stdin);
+			scanf("%c",&respuesta);
+		}while(respuesta == 's' && limite > 0);
+		retorno = i;
+	}
+	return retorno;
+}
 
 
+int maximoArrayInt(int array[],int limite,int *pResultado)
+{
+	int i;
+	int retorno = -1;
+	int maximo;
+	int posMaximo;
+	if(array != NULL && limite > 0)
+	{
+		retorno = 0;
+		maximo = array[0];
+		posMaximo = 0;
+		for(i=1;i<limite;i++)
+		{
+			if(array[i]>maximo)
+			{
+				maximo = array[i];
+				posMaximo=i;
+			}
+		}
+		*pResultado = posMaximo;
+	}
+	return retorno;
+}
 
-
-
-
+int ordenarArrayInt(int array[],int limite)
+{
+	int i;
+	int retorno = -1;
+	int posMaximo;
+	int auxiliar;
+	if(array != NULL && limite > 0)
+	{
+		for(i=0;i<limite;i++)
+		{
+			maximoArrayInt(array+i,limite-i,&posMaximo);
+			auxiliar = array[i];
+			array[i]=array[posMaximo+i];
+			array[posMaximo+i] = auxiliar;
+		}
+	}
+	return retorno;
+}
 
