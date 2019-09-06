b6be5ba90b865ea0df31084d5c0b6793e76a3450
profesor <profesor@LAB00PC00>
2019-09-05 21:21:39 -0300

    Agregamos getString fix

--- Clase_05/src/Clase_05.c
+++ Clase_05/src/Clase_05.c
@@ -250,21 +250,28 @@ int getString(	char *pResultado,
 {
 	int retorno = -1;
 	char buffer[4096];
-	do
+	if(	pResultado != NULL &&
+		pMensaje != NULL &&
+		pMensajeError != NULL &&
+		minimo <= maximo &&
+		reintentos >= 0)
 	{
-		printf("%s",pMensaje);
-		__fpurge(stdin);
-		fgets(buffer,sizeof(buffer),stdin);
-		buffer[strlen(buffer)-1] = '\0';
-		if(strlen(buffer)>=minimo && strlen(buffer) <= maximo)
-		{
-			strncpy(pResultado,buffer,maximo+1);
-			retorno = 0;
-			break;
-		}
-		printf("%s",pMensajeError);
-		reintentos--;
-	}while(reintentos >= 0);
+		do
+			{
+				printf("%s",pMensaje);
+				__fpurge(stdin);
+				fgets(buffer,sizeof(buffer),stdin);
+				buffer[strlen(buffer)-1] = '\0';
+				if(strlen(buffer)>=minimo && strlen(buffer) <= maximo)
+				{
+					strncpy(pResultado,buffer,maximo+1);
+					retorno = 0;
+					break;
+				}
+				printf("%s",pMensajeError);
+				reintentos--;
+			}while(reintentos >= 0);
+	}
 	return retorno;
 }
 
