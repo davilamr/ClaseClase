1886e3ac0c81fc4283fb7beddfe3c2a2a31a7632
profesor <profesor@LAB00PC00>
2019-09-12 20:26:54 -0300

    Clase 8

--- Clase_08/src/Clase_08.c
+++ Clase_08/src/Clase_08.c
@@ -8,25 +8,6 @@
  ============================================================================
  */
 
-/*
- ============================================================================
- Name        : Clase_05.c
- Author      :
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
-
-  Hacer un programa con el siguiente menu:
-   1) ingresar nuevo nombre (no acepta nombres repetidos)
-   2) listar todo
-   3) ordenar por nombre
-   4) Borrar nombre. Se ingresa el nombre y si se encuentra se elimina de la lista
-
-	El programa permitira ingresar hasta 100 nombres.
-
- */
-
 #include <stdio.h>
 #include <stdio_ext.h>
 #include <stdlib.h>
@@ -35,36 +16,126 @@
 #define QTY_EMPLEADOS 10
 #define QTY_NOMBRES 10
 #define QTY_CARACTERES 50
+#define STATUS_EMPTY 0
+#define STATUS_NOT_EMPTY 1
+
+struct sEmpleado
+{
+	int idEmpleado;
+	int status;
+	//***************
+	char nombre[50];
+	char apellido[50];
+};
+
 
 int imprimirArrayString(char aNombres[][QTY_CARACTERES], int cantidad);
+int ordenarArrayString(char aNombres[][QTY_CARACTERES], int cantidad);
+int imprimirArrayEmpleados(struct sEmpleado *aEmpleado, int cantidad);
+
+
 
 int main(void)
 {
-	char aNombres[QTY_NOMBRES][QTY_CARACTERES];
-	char nombre[50];
+
+	struct sEmpleado aEmpleados[1000];
+	struct sEmpleado bEmpleado;
 	int i;
+	int idEmpleado = 0;
 
-	for(i=0;i<5;i++){
-		getString(nombre,"Ingrese el nombre",
+	for(i=0;i<3;i++){
+
+		getString(bEmpleado.nombre,"Ingrese el nombre",
 				"ERROR", 1, 49, 2);
-		strcpy(aNombres[i], nombre);
-		printf("%s",aNombres[i]);
-	}
 
-	imprimirArrayString(aNombres,QTY_NOMBRES);
+		getString(bEmpleado.apellido,"Ingrese el apellido",
+						"ERROR", 1, 49, 2);
+
+		bEmpleado.idEmpleado = idEmpleado;
+		idEmpleado++;
+		bEmpleado.status = STATUS_NOT_EMPTY;
+
+		aEmpleados[i] = bEmpleado;
+	}
+	imprimirArrayEmpleados(aEmpleados,3);
 
 	return EXIT_SUCCESS;
+}
+
+int imprimirArrayEmpleados(struct sEmpleado *aEmpleado, int cantidad){
+	int i;
+	int retorno = -1;
+	if(aEmpleado != NULL && cantidad>0)
+	{
+		retorno = 0;
+		for(i=0;i<cantidad;i++)
+		{
+			printf("Id: %d - Nombre: %s - Apellido: %s \n",aEmpleado[i].idEmpleado,aEmpleado[i].nombre,aEmpleado[i].apellido);
+		}
+	}
+	return retorno;
 }
 
 int imprimirArrayString(char aNombres[][QTY_CARACTERES], int cantidad){
 	int i;
 	int retorno = -1;
-	if(aNombres != NULL && cantidad>0){
+	if(aNombres != NULL && cantidad>0)
+	{
 		retorno = 0;
-		for(i=0;i<cantidad;i++){
+		for(i=0;i<cantidad;i++)
+		{
 			printf("Nombre: %s \n",aNombres[i]);
 		}
 	}
 	return retorno;
 }
+
+int ordenarArrayString(char aNombres[][QTY_CARACTERES], int cantidad){
+	int i;
+	int retorno = -1;
+	char buffer[QTY_CARACTERES];
+	int fSwap;
+	if(aNombres != NULL && cantidad>0)
+	{
+		retorno = 0;
+		do
+		{
+			fSwap = 0;
+			for(i=0;i<cantidad-1;i++)
+			{
+				if(strncmp(aNombres[i],aNombres[i+1],QTY_CARACTERES)>0)
+				{
+					fSwap = 1;
+					strncpy(buffer,aNombres[i],QTY_CARACTERES);
+					strncpy(aNombres[i],aNombres[i+1],QTY_CARACTERES);
+					strncpy(aNombres[i+1],buffer,QTY_CARACTERES);
+				}
+			}
+		}while(fSwap);
+	}
+	return retorno;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
